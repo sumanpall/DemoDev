691efe4bcac5d7e8012b790b0bad09e58ac527c0
Palle <suman.palle@accenture.com>
2018-09-04 12:45:59 +0530

    Adding the for loop to hello java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello{
 
   public static void main(String[] args){
-    System.out.println("Hello world...");
 
+  for(int i=0;i<=10;i++){
+    System.out.println("Hello world..."+i);
+   }
 }
 
 }
